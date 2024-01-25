5ddc8bae60b52c13b9eb9ca6d96686b52eef58d5
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-01-25 18:14:46 -0500

    Vibration button setup

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View, Text, Button, Vibrate } from 'react-native';
+import * as Haptics from 'expo-haptics';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Haptics.selectionAsync</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Selection" onPress={() => Vibrate.vibrate(100)} />
+      </View>
+      <Text style={styles.text}>Haptics.notificationAsync</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Success"
+          onPress={
+            () =>
+              Haptics.notificationAsync(
+                Haptics.NotificationFeedbackType.Success
+              )
+          }
+        />
+        <Button
+          title="Error"
+          onPress={
+            () =>
+              Haptics.notificationAsync(
+                Haptics.NotificationFeedbackType.Error
+              )
+          }
+        />
+        <Button
+          title="Warning"
+          onPress={
+            () =>
+              Haptics.notificationAsync(
+                Haptics.NotificationFeedbackType.Warning
+              )
+          }
+        />
+      </View>
+      <Text style={styles.text}>Haptics.impactAsync</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Light"
+          onPress={
+            () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+          }
+        />
+        <Button
+          title="Medium"
+          onPress={
+            () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+          }
+        />
+        <Button
+          title="Heavy"
+          onPress={
+            () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
+          }
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +67,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 10,
+    marginBottom: 30,
+    justifyContent: 'space-between',
   },
 });
