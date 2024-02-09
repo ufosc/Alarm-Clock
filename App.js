90cb0440947a22c91d66a9ad3faecd5248c0f377
Sebastian <csebastianpaez@outlook.com>
2024-02-09 00:10:05 -0500

    Add initial footer tab

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, ScrollView, TouchableOpacity, View } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import AlarmClock from './screens/Alarm';
+import FooterTab from './components/FooterTab';
 import { styles, textStyles } from './styles';  // Adjust the path as needed
 
 
@@ -20,28 +21,31 @@ export default function App() {
   }, []);
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.page}>
+      <ScrollView style={styles.container}>
 
 
-      <Text style={{ ...textStyles.titleText, padding: 20, paddingTop: 100 }}>Alarm Clock</Text>
-      <View style={styles.box}>
+        <Text style={{ ...textStyles.titleText, padding: 20, paddingTop: 100 }}>Alarm Clock</Text>
+        <View style={styles.box}>
 
-        <Text style={styles.time}>{currentTime.toLocaleTimeString()}</Text>
-      </View>
-      <TouchableOpacity onPress={() => setShowWow(!showWow)} style={styles.button}>
-        <Text style={textStyles.buttonText}>Button</Text>
-      </TouchableOpacity>
-    
-
-      {showWow && (
-        <Text style={{ padding: 40 }}>Wow it actually works!</Text>
-      )}
+          <Text style={styles.time}>{currentTime.toLocaleTimeString()}</Text>
+        </View>
+        <TouchableOpacity onPress={() => setShowWow(!showWow)} style={styles.button}>
+          <Text style={textStyles.buttonText}>Button</Text>
+        </TouchableOpacity>
 
 
-<AlarmClock style={{marginTop: 100, padding: 100, paddingTop: 100 }}/>
+        {showWow && (
+          <Text style={{ padding: 40 }}>Wow it actually works!</Text>
+        )}
 
 
-      <StatusBar style="auto" />
-    </ScrollView>
+        <AlarmClock style={{marginTop: 100, padding: 100, paddingTop: 100 }}/>
+
+
+        <StatusBar style="auto" />
+        </ScrollView>
+        <FooterTab />
+    </View>
   );
 }
