f9bae402e04f1f18064aa14bc1d8f5674ecb36f8
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-02-20 18:38:13 -0500

    Allow for multiple alarms at once

--- screens/Alarms.jsx
+++ screens/Alarms.jsx
@@ -7,10 +7,7 @@ import { textStyles, styles } from '../styles/styles';
 
 export default function Alarms() {
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [alarmTime, setAlarmTime] = useState(null);
-  const [isAlarmSet, setIsAlarmSet] = useState(false);
-  const [countdown, setCountdown] = useState('');
-  const [isAlarmOn, setIsAlarmOn] = useState(true);
+  const [alarms, setAlarms] = useState([]);
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -21,32 +18,39 @@ export default function Alarms() {
   };
 
   const handleConfirm = (date) => {
-    setAlarmTime(date);
-    setIsAlarmSet(true);
 
     // Set a timeout to trigger the alarm
     const currentTime = new Date();
     const timeUntilAlarm = date - currentTime;
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       alert('Wake up!');
-      setIsAlarmSet(false);
-      setCountdown('');
+      // setIsAlarmSet(false);
+      // setCountdown('');
     }, timeUntilAlarm);
 
+    const newAlarm = {
+      date,
+      timeout,
+      enabled: true,
+    };
+
+    setAlarms(alarms.concat([newAlarm]));
+    console.log(alarms);
+
     // Start updating the countdown every second
-    const intervalId = setInterval(() => {
-      const remainingTime = date - new Date();
-      if (remainingTime <= 0) {
-        clearInterval(intervalId);
-        setCountdown('');
-      } else {
-        const minutes = Math.floor(remainingTime / 60000);
-        const formatMinutes = String(minutes).padStart(2, '0');
-        const seconds = Math.floor((remainingTime % 60000) / 1000);
-        const formatSeconds = String(seconds).padStart(2, '0');
-        setCountdown(`${formatMinutes} min : ${formatSeconds} sec`);
-      }
-    }, 1000);
+    // const intervalId = setInterval(() => {
+    //   const remainingTime = date - new Date();
+    //   if (remainingTime <= 0) {
+    //     clearInterval(intervalId);
+    //     setCountdown('');
+    //   } else {
+    //     const minutes = Math.floor(remainingTime / 60000);
+    //     const formatMinutes = String(minutes).padStart(2, '0');
+    //     const seconds = Math.floor((remainingTime % 60000) / 1000);
+    //     const formatSeconds = String(seconds).padStart(2, '0');
+    //     setCountdown(`${formatMinutes} min : ${formatSeconds} sec`);
+    //   }
+    // }, 1000);
 
     hideDatePicker();
   };
@@ -70,36 +74,41 @@ export default function Alarms() {
         isVisible={isDatePickerVisible}
         mode="time"
         date={new Date()}
-        onConfirm={handleConfirm}
-        onCancel={hideDatePicker}
+        onConfirm={(d) => handleConfirm(d)}
+        onCancel={() => hideDatePicker()}
       />
       {/* <Text>{isAlarmOn ? 'Alarm Status: On' : isAlarmSet? 'Alarm Status: Off' : ''}</Text> */}
       {/* {isAlarmSet && <Text>Time Left: {countdown}</Text>} */}
-      {isAlarmSet && (
-        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
-          <View style={styles.alarmTimeBox}>
-            <Text style={textStyles.timeText}>
-              {alarmTime.toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: true,
-              })}{' '}
-            </Text>
+
+      {alarms &&
+        alarms.map((a) => (
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+            <View style={styles.alarmTimeBox}>
+              <Text style={textStyles.timeText}>
+                {a.date.toLocaleTimeString([], {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  hour12: true,
+                })}{' '}
+              </Text>
+            </View>
+
+            {/* This switch doesn't do anything yet but it totally could! */}
+            <Switch
+              style={{ marginLeft: 100 }}
+              trackColor={{ false: '#767577', true: 'lightgreen' }}
+              thumbColor={a.enabled ? 'f4f3f4' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => {
+                // eslint-disable-next-line no-param-reassign
+                a.enabled = !a.enabled;
+              }}
+              value={a.enabled}
+            />
           </View>
+        ))}
 
-          {/* This switch doesn't do anything yet but it totally could! */}
-          <Switch
-            style={{ marginLeft: 100 }}
-            trackColor={{ false: '#767577', true: 'lightgreen' }}
-            thumbColor={isAlarmOn ? 'f4f3f4' : '#f4f3f4'}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={() => setIsAlarmOn(!isAlarmOn)}
-            value={isAlarmOn}
-          />
-        </View>
-      )}
       <StatusBar style={{ marginTop: 500 }} />
-
     </View>
   );
 }
