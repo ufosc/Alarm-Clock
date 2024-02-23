6f3c2c6649b12f524f1a6c1051d019e7d9c00edc
taiphlosion <ttai8501@gmail.com>
2024-02-23 01:15:20 -0500

    Implemented feature to add world clock

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -6,6 +6,8 @@ import { useNavigation } from '@react-navigation/native';
 import AlarmClock from '../components/AlarmClock';
 import { styles, textStyles } from '../styles/styles';
 import AlarmCard from '../components/AlarmCard';
+import moment from 'moment-timezone';
+import { Picker } from '@react-native-picker/picker';
 
 export default function Home({ isDarkMode }) {
   // Accept isDarkMode as a prop
@@ -13,6 +15,10 @@ export default function Home({ isDarkMode }) {
   const [currentTime, setCurrentTime] = useState(new Date());
   const [alarms, setAlarms] = useState([]);
   const [selectedAlarm, setSelectedAlarm] = useState(null);
+  const [selectedTimeZone, setSelectedTimeZone] = useState(moment.tz.guess());
+  const [screenClock, setScreenClock] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const timeZones = moment.tz.names();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -25,6 +31,21 @@ export default function Home({ isDarkMode }) {
   const dynamicStyles = {
     backgroundColor: isDarkMode ? 'darkgrey' : 'white',
     color: isDarkMode ? 'white' : 'black',
+  };
+
+  
+  const addNewTimeZone = (zone) =>{
+    if (!screenClock.includes(zone)){
+      setScreenClock([...screenClock, zone]);
+    }
+  }
+
+  const removeTimeZone = (index) => {
+    setScreenClock(screenClock => screenClock.filter((_, i) => i !== index));
+  };
+
+  const showTimeZonePicker = () => {
+    setModalVisible(!modalVisible);
   };
 
   // Function to handle saving alarm data
@@ -96,6 +117,57 @@ export default function Home({ isDarkMode }) {
           onDelete={handleDeleteAlarm}
         />
       ))}
+
+      <Text style={{ ...textStyles.titleText, padding: 20, color: dynamicStyles.color }}>
+        World Clock
+      </Text>
+
+      <View style={{ height: 10 }} />
+        {/* Managing picker for the time zones */}
+        <View>
+          <TouchableOpacity onPress={() =>{
+            showTimeZonePicker();
+            if (modalVisible) {
+              addNewTimeZone(selectedTimeZone); // Add the selected time zone
+            }        
+          }} 
+          style={styles.button}>
+            <Text style={textStyles.buttonText}>
+              {modalVisible ? "Confirm Clock" : "Add New World Clock"} 
+            </Text>
+          </TouchableOpacity>     
+          {modalVisible && (
+            <Picker
+              style={styles.picker}
+              selectedValue={selectedTimeZone}
+              onValueChange={(itemValue) => {
+                setSelectedTimeZone(itemValue);
+              }}
+            >
+            {timeZones.map((zone) => (
+              <Picker.Item key = {zone} label = {zone} value = {zone}/>
+            ))}
+            </Picker>
+          )}     
+        </View>
+{/* Displays different timezones */}
+        <View>
+          {screenClock.map((zone, index) => (
+            <View key={index} style={[styles.time, { backgroundColor: dynamicStyles.backgroundColor }]}>
+              <Text style={{ ...styles.buttonText, color: dynamicStyles.color }}>
+                {zone}
+              </Text>
+              <TouchableOpacity onPress={() => removeTimeZone(index)} style={styles.deleteButton}>
+                <Text style={styles.deleteButtonText}>Delete</Text>
+              </TouchableOpacity>
+              <View style={[styles.box, { backgroundColor: dynamicStyles.backgroundColor }]}>
+                <Text style={{ ...styles.time, color: dynamicStyles.color }}>
+                  {moment().tz(zone).format('hh:mm:ss A')}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </View>
 
       <View style={{ height: 200 }} />
     </ScrollView>
